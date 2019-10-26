0a2e96933415276958390e275ce7f3f8e1fbdd6b
Certinax <s319217@oslomet.no>
2019-10-26 02:44:42 -0700

    Lab6 completed

--- app.js
+++ app.js
@@ -10,13 +10,26 @@ app.get("/", function(req, res) {
 });
 
 app.get("/mercury", function(req, res) {
-  res.send("This will be mercury webp!");
+  res.render("mercury.html");
 });
 app.get("/venus", function(req, res) {
-  res.send("This will be Venus webp");
+  res.render("venus.html");
+});
+app.get("/earth", function(req, res) {
+  res.render("earth.html");
+});
+app.get("/saturn", function(req, res) {
+  res.render("saturn.html");
+});
+app.get("/mars", function(req, res) {
+  res.render("mars.html");
 });
 
+
 // server listener
-app.listen(process.env.PORT, process.env.IP, function() {
-  console.log("Express server is Running...");
+var port = (process.env.PORT || "3000");
+var ip = (process.env.IP || "localhost");
+
+app.listen(port, ip, function() {
+  console.log("Express server is Running on port: ", port);
 });
